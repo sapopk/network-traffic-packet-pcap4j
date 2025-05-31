c0bfb723513c1c3c8ec4138d80818e93395fb4c8
sapopk <jooramos1@gmail.com>
2025-05-31 13:47:19 +0100

    Add and Capture TCP packet to logger.

--- src/main/java/project/NetworkInterfaces/Service/NetworkService.java
+++ src/main/java/project/NetworkInterfaces/Service/NetworkService.java
@@ -14,12 +14,16 @@ import org.pcap4j.core.Pcaps;
 import org.pcap4j.packet.EthernetPacket;
 import org.pcap4j.packet.IpV4Packet;
 import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.TcpPacket;
 
 import project.NetworkInterfaces.Packet.PacketLogger;
 import project.NetworkInterfaces.Packet.Ethernet.EthernetHeader;
+import project.NetworkInterfaces.Packet.HEXStream.HexStream;
 import project.NetworkInterfaces.Packet.IPV4.IPV4Flag;
 import project.NetworkInterfaces.Packet.IPV4.IPV4Header;
 import project.NetworkInterfaces.Packet.IPV4.TypeOfService;
+import project.NetworkInterfaces.Packet.Protocol.TCPFlag;
+import project.NetworkInterfaces.Packet.Protocol.TCPHeader;
 
 public class NetworkService {
     private List<PacketLogger> packetLoggers = new CopyOnWriteArrayList<>();
@@ -35,24 +39,26 @@ public class NetworkService {
                 List<PcapNetworkInterface> listOfAllInterfaces = Pcaps.findAllDevs();
                 PcapNetworkInterface interfaceDevices = null;
 
-                for(PcapNetworkInterface listOfDevs : listOfAllInterfaces) {
-                    for(PcapAddress address : listOfDevs.getAddresses()) {
-                        if(address.getAddress() instanceof InetAddress && address.getAddress().getHostAddress().equals(targetIPAddress)) {
+                for (PcapNetworkInterface listOfDevs : listOfAllInterfaces) {
+                    for (PcapAddress address : listOfDevs.getAddresses()) {
+                        if (address.getAddress() instanceof InetAddress
+                                && address.getAddress().getHostAddress().equals(targetIPAddress)) {
                             interfaceDevices = listOfDevs;
                             break;
                         }
                     }
 
-                    if(interfaceDevices != null) {
+                    if (interfaceDevices != null) {
                         break;
                     }
                 }
 
-                if(interfaceDevices == null) {
+                if (interfaceDevices == null) {
                     System.err.println("No interface device has been found for address: " + targetIPAddress);
                 }
 
-                PcapHandle handler = interfaceDevices.openLive(65536, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, 1000);
+                PcapHandle handler = interfaceDevices.openLive(65536, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS,
+                        1000);
 
                 PacketListener packetListener = packet -> {
                     PacketLogger logger = new PacketLogger();
@@ -72,16 +78,14 @@ public class NetworkService {
         }).start();
     }
 
-
     public void setEthernetHeader(Packet packet, PacketLogger logger) {
         EthernetPacket ethernetPacket = packet.get(EthernetPacket.class);
 
-        if(ethernetPacket != null) {
+        if (ethernetPacket != null) {
             EthernetHeader ethernetHeader = new EthernetHeader(
-                ethernetPacket.getHeader().getSrcAddr().toString(),
-                ethernetPacket.getHeader().getDstAddr().toString(),
-                ethernetPacket.getHeader().getType().toString()
-            );
+                    ethernetPacket.getHeader().getSrcAddr().toString(),
+                    ethernetPacket.getHeader().getDstAddr().toString(),
+                    ethernetPacket.getHeader().getType().toString());
 
             logger.setEthernetHeader(ethernetHeader);
         }
@@ -90,38 +94,84 @@ public class NetworkService {
     public void setIPv4Header(Packet packet, PacketLogger logger) {
         IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
 
-        if(ipV4Packet != null) {
-            
+        if (ipV4Packet != null) {
+
             String checkSumvalue = "0x" + Integer.toHexString(ipV4Packet.getHeader().getHeaderChecksum());
 
             IPV4Flag ipv4Flag = new IPV4Flag(
-                ipV4Packet.getHeader().getReservedFlag(),
-                ipV4Packet.getHeader().getDontFragmentFlag(),
-                ipV4Packet.getHeader().getMoreFragmentFlag()
-            );
+                    ipV4Packet.getHeader().getReservedFlag(),
+                    ipV4Packet.getHeader().getDontFragmentFlag(),
+                    ipV4Packet.getHeader().getMoreFragmentFlag());
 
             TypeOfService typeOfService = new TypeOfService(
-                ipV4Packet.getHeader().getTos().value(),
-                ipV4Packet.getHeader().getTos().value(),
-                ipV4Packet.getHeader().getTos().value()
-            );
+                    ipV4Packet.getHeader().getTos().value(),
+                    ipV4Packet.getHeader().getTos().value(),
+                    ipV4Packet.getHeader().getTos().value());
 
             IPV4Header ipv4Header = new IPV4Header(
-                ipV4Packet.getHeader().getVersion().value(),
-                ipV4Packet.getHeader().getIhl(),
-                ipV4Packet.getHeader().getTotalLength(),
-                ipV4Packet.getHeader().getIdentificationAsInt(),
-                ipV4Packet.getHeader().getFragmentOffset(),
-                ipV4Packet.getHeader().getTtl(),
-                ipV4Packet.getHeader().getProtocol().value(),
-                checkSumvalue,
-                ipV4Packet.getHeader().getSrcAddr().getHostAddress(),
-                ipV4Packet.getHeader().getDstAddr().getHostAddress(),
-                ipv4Flag,
-                typeOfService
-            );
+                    ipV4Packet.getHeader().getVersion().value(),
+                    ipV4Packet.getHeader().getIhl(),
+                    ipV4Packet.getHeader().getTotalLength(),
+                    ipV4Packet.getHeader().getIdentificationAsInt(),
+                    ipV4Packet.getHeader().getFragmentOffset(),
+                    ipV4Packet.getHeader().getTtl(),
+                    ipV4Packet.getHeader().getProtocol().value(),
+                    checkSumvalue,
+                    ipV4Packet.getHeader().getSrcAddr().getHostAddress(),
+                    ipV4Packet.getHeader().getDstAddr().getHostAddress(),
+                    ipv4Flag,
+                    typeOfService);
 
             logger.setIpv4Header(ipv4Header);
         }
+    }
+
+    public void setTCPPacket(Packet packet, PacketLogger logger) {
+        TcpPacket tcpPacket = packet.get(TcpPacket.class);
+
+        if (tcpPacket != null) {
+
+            String checkSumValue = "0x" + Integer.toHexString(tcpPacket.getHeader().getChecksum());
+
+            TCPFlag tcpFlag = new TCPFlag(
+                    tcpPacket.getHeader().getUrg(),
+                    tcpPacket.getHeader().getAck(),
+                    tcpPacket.getHeader().getPsh(),
+                    tcpPacket.getHeader().getRst(),
+                    tcpPacket.getHeader().getSyn(),
+                    tcpPacket.getHeader().getFin());
+
+            TCPHeader tcpHeader = new TCPHeader(
+                    tcpPacket.getHeader().getSrcPort().valueAsInt(),
+                    tcpPacket.getHeader().getDstPort().valueAsInt(),
+                    tcpPacket.getHeader().getSequenceNumber(),
+                    tcpPacket.getHeader().getAcknowledgmentNumber(),
+                    tcpPacket.getHeader().getDataOffset(),
+                    tcpPacket.getHeader().getReserved(),
+                    tcpPacket.getHeader().getWindow(),
+                    checkSumValue,
+                    tcpPacket.getHeader().getUrgentPointer(),
+                    tcpFlag);
+
+            HexStream hexStream = new HexStream();
+            if (tcpPacket.getPayload() != null) {
+                byte[] pureData = tcpPacket.getPayload().getRawData();
+                hexStream.setLength(pureData.length);
+                hexStream.setHexStream(defineBytesToHexStream(pureData));
+            }
+
+            logger.setTcpHeader(tcpHeader);
+            logger.setHexStream(hexStream);
+        }
+    }
+
+    public String defineBytesToHexStream(byte[] bytes) {
+        StringBuffer builder = new StringBuffer();
+
+        for (byte b : bytes) {
+            builder.append(String.format("%20x", b)).append(" ");
+        }
+
+        return builder.toString().trim();
     }
 }
